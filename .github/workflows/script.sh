b85c83c7bd3fa8322b17e0965902a1e1b6034f44
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-09-20 13:06:27 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -2,5 +2,5 @@
 echo "Printing text with newline"
 echo -n "Printing text without newline"
 echo -e "\nRemoving \t backslash \t characters\n"
-IMAGE_VERSION=$(grep '##' ${GITHUB_REPOSITORY}/CHANGELOG.md | head -n 1 | cut -d ' ' -f 2)
+IMAGE_VERSION=$(grep '##' ${GITHUB_REPOSITORY}/Changelog.md | head -n 1 | cut -d ' ' -f 2)
 echo "IMAGE_VERSION: $IMAGE_VERSION"
