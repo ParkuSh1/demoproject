c147fd587f87ef69cb7c531486f23cfdee3384f5
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-07 11:44:32 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,54 +1,10 @@
 #!/bin/bash
 
-echo "Printing text with newline"
-echo -n "Printing text without newline"
-echo -e "\nRemoving \t backslash \t characters\n"
-
-@echo off
-set "projectdir=%1"
-
-setlocal enabledelayedexpansion
-set "file1=${GITHUB_WORKSPACE}/.github/workflows/script.sh"
-set i=0
-set "_SUBSTR=##"
-
-echo %file1%
-for /F "delims=" %%a in (%file1%) do (
-    set /A i+=1
-    set "array[!i!]=%%a"
-)
-
-rem below loop to print the file content
-rem for /L %%A in (1,1,%i%) do echo !array[%%A]!
-
-rem below loop search fo ## pattern
-for /L %%A in (1,1,%i%) do (  
-  rem echo !array[%%A]!                      
-  if not "!array[%%A]!"=="!array[%%A]:%_SUBSTR%=!" (
-            set y=!array[%%A]!
-            rem print the matched string ##
-            rem echo !y!
-	    call :extract !array[%%A]! 
-            rem echo !ChangeLogVersion!
-            goto End
-        ) 
-        
-)
-
-:extract
-set tempstr=%*
-set tempstr=%tempstr:[=@%
-set tempstr=%tempstr:]=@%
-for /F "tokens=2 delims=@ " %%a in ("%tempstr%") do (
-   set ChangeLogVersion=%%a
-)
-EXIT /B 0
-
-
-:End
-rem echo ChangeLogVersion: %ChangeLogVersion%
-endlocal & set ChangeLogVersion=%ChangeLogVersion%
-echo %ChangeLogVersion%  
-exit /B %ERRORLEVEL%
-
-
+# example of using arguments to a script
+echo "My first name is $1"
+echo "My surname is $2"
+echo "Total number of arguments is $#" 
+var=$((3+9))
+echo $var
+@echo off 
+echo "Hello World"
