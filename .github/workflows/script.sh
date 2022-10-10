4882de344fe1db0a15a5e732a48bfedad4755123
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-10 13:26:01 +0200

    added script in main.yml file

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # example of using arguments to a script
 echo "My first name is $1"
@@ -6,5 +6,8 @@ echo "My surname is $2"
 echo "Total number of arguments is $#" 
 var=$((4+9))
 echo $var
-@echo off 
+echo df
+df
+echo env
+env
 echo "Hello World"
