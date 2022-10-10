14432d0f85a041d757c056f89818378026c892de
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-10 13:45:39 +0200

    changed

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# example of using arguments to a script
-echo "My first name is $1"
-echo "My surname is $2"
-echo "Total number of arguments is $#" 
-var=$((4+9))
-echo $var
-echo df
-df
-echo env
-env
-echo "Hello World"
+set "projectdir=$1"
+
+set "file1=%projectdir%\\..\\CHANGELOG.md"
+set i=0
+set "_SUBSTR=##"
+
+for /F "delims=" %%a in (%file1%) do (
+    set /A i+=1
+    set "array[!i!]=%%a"
+)
+
+rem below loop to print the file content
+for /L %%A in (1,1,%i%) do echo !array[%%A]!
