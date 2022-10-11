e5315da483fd9b9ad0ae69ee893e347ba60a9ee3
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-11 12:28:09 +0200

    commit

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,11 +1,10 @@
-#!usr/bin/env bash
-
 function myfunc()
 {
-    local  myresult=$(grep -m1 '##' $1/Changelog.md | \
-  awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}')
-    echo "$myresult"
+    local  __resultvar=$1
+    local  myresult='some value'
+    eval $__resultvar="'$myresult'"
 }
+
 
 
 #grep -m1 '##' $1/Changelog.md | \
