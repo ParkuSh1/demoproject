6d6faad730168fd884b4eff7be25e6cdb61a5e51
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-11 12:40:23 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -8,9 +8,6 @@ input=$1/Changelog.md
 $2=$(grep -m1 '##' $1/Changelog.md | \
   awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}')
 
-echo $2
-
-
 #grep -m1 '##' $1/Changelog.md | \
 #  awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}'
 
