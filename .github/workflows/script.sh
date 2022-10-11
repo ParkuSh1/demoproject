5812cd8dd92061188b7a3b913b7b582961aeeef2
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-11 15:17:58 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -9,7 +9,7 @@ output=$1/logout.txt
 ChangeLogVersion=$(grep -m1 '##' $1/Changelog.md | \
   awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}')
 
-if [ ! -f $1/Changelog.md ]
+if [ ! -f $1/demo.txt]
 then
 	echo "File not found"
 fi
