c2265d3b5d866cafef2b73dbb11f1ee47dacbb0a
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-11 12:10:58 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -4,20 +4,9 @@
 echo "The first argument is $1."
 
 input=$1/Changelog.md
-######################################
-# $IFS removed to allow the trimming # 
-#####################################
-ReadVersion()
-{
+
 ChangeLogVersion=$(grep -m1 '##' $1/Changelog.md | \
   awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}')
-
-echo $ChangeLogVersion
-return $ChangeLogVersion
-}
-
-#ChangeLogVersion=$(grep -m1 '##' $1/Changelog.md | \
-#  awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}')
 
 #echo $ChangeLogVersion
 #grep -m1 '##' $1/Changelog.md | \
