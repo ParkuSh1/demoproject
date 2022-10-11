87bc0057856cbe003255c25a0374eb6d93ce5e55
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-11 12:12:05 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -8,7 +8,7 @@ input=$1/Changelog.md
 ChangeLogVersion=$(grep -m1 '##' $1/Changelog.md | \
   awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}')
 
-#echo $ChangeLogVersion
+echo $ChangeLogVersion
 #grep -m1 '##' $1/Changelog.md | \
 #  awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}'
 
