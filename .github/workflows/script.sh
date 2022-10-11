2af3722dbbceee5dab89da55140c97ba2b7b6566
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-11 11:27:12 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -7,7 +7,10 @@ input=$1/Changelog.md
 ######################################
 # $IFS removed to allow the trimming # 
 #####################################
-while read -r line
-do
-  echo "$line"
-done < "$input"
+grep -m1 '##' $1/Changelog.md | \
+  awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}'
+
+#while read -r line
+#do
+# echo "$line"
+#done < "$input"
