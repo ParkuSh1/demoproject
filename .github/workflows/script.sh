c20f03a64afbd53d178e31f7a915eafa22ad5f18
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-11 13:46:24 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -2,13 +2,15 @@
 
 #set "projectdir=$1"
 echo "$1."
+LOG="./test.log"
+rm -f $LOG
 
 input=$1/Changelog.md
 
 ChangeLogVersion=$(grep -m1 '##' $1/Changelog.md | \
   awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}')
 
-echo $ChangeLogVersion
+echo $ChangeLogVersion | tee $LOG 2>&1
 #grep -m1 '##' $1/Changelog.md | \
 #  awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}'
 
