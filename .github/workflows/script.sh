059fa8cd063c23428cb6c536aecb2a719c33ec2c
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-11 12:07:52 +0200

    file changed

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -7,10 +7,19 @@ input=$1/Changelog.md
 ######################################
 # $IFS removed to allow the trimming # 
 #####################################
+ReadVersion()
+{
 ChangeLogVersion=$(grep -m1 '##' $1/Changelog.md | \
   awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}')
 
 echo $ChangeLogVersion
+return $ChangeLogVersion
+}
+
+#ChangeLogVersion=$(grep -m1 '##' $1/Changelog.md | \
+#  awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}')
+
+#echo $ChangeLogVersion
 #grep -m1 '##' $1/Changelog.md | \
 #  awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}'
 
