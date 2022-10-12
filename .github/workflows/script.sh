b2b3f51a53e76176abc18b56f478a378e5f4f7af
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-12 12:05:26 +0200

    dummy

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,8 +1,8 @@
 #!usr/bin/bash
 
 #set "projectdir=$1"
-echo "$1."
-ChangeLogVersion=$(grep -m1 '##' $1/../CHANGELOG.md | \
+echo $1
+ChangeLogVersion=$(grep -m1 '##' $1/CHANGELOG.md | \
   awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}')
 
 echo $ChangeLogVersion
