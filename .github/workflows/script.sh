2e10c92a18cadf873cf914b5d4cd5248d5f02a3c
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-10-13 14:36:53 +0200

    dummy

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -3,6 +3,12 @@
 #set "projectdir=$1"
 echo $1
 
+str='some text with in it'
+if [[ $str == *['!'@#\$%^\&*()_+]* ]]
+then
+  echo "It contains one of those"
+fi
+
 ChangeLogVersion=$(grep -m1 '##' $1/CHANGELOG.md | \
   awk '{for(i=1; i<=NF; i++) if($i~/##/) print $(i+1)}')
 
