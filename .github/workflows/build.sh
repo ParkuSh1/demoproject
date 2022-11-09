a071bfc31a204159bd72122ea1401e294ad4794a
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-11-09 06:44:06 +0100

    re

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,6 +5,6 @@ echo $1
 which bash
 ls
 chmod +x $1/.github/workflows/arm-none-eabi-size.exe
-$1/.github/workflows/arm-none-eabi-size.exe Build/BF36_H753IIT6_BL/HMI_Projekt.elf
+$1/.github/workflows/arm-none-eabi-size.exe $1/Build/BF36_H753IIT6_BL/HMI_Projekt.elf
 
 echo $output
