0190a100118e0865453a33f24af2383d15b2918f
ParkuSh1 <104121953+ParkuSh1@users.noreply.github.com>
2022-11-09 06:35:13 +0100

    Update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,7 +1,9 @@
 #!usr/bin/bash
 
+echo $1
+
 which bash
 ls
-arm-none-eabi-size.exe Build/BF36_H753IIT6_BL/HMI_Projekt.elf
+$1/.github/workflows/arm-none-eabi-size.exe Build/BF36_H753IIT6_BL/HMI_Projekt.elf
 
 echo $output
